d632bd3456cd71d914c8ac1d24dac1b1d0e7057e
Clarchik <clarchik.services@gmail.com>
2025-02-20 16:52:32 +0100

    fefactor: fix

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,20 +10,19 @@ import {
   declarationsESLint
 } from './tools/eslint-setup/index.mjs';
 
-export default tseslint
-  .config
-  // nx.configs['flat/base'],
-  // nx.configs['flat/typescript'],
-  // nx.configs['flat/javascript'],
-  // nx.configs['flat/angular'],
-  // plugins,
-  // angularESLint,
-  // angularESLintTemplate,
-  // declarationsESLint,
-  // vitestESLint,
-  // storyBookESLint,
-  // nxLintBoundaries,
-  // {
-  //   ignores: ['**/dist', 'node_modules/', '**/.nx', '**/.angular']
-  // }
-  ();
+export default tseslint.config(
+  nx.configs['flat/base'],
+  nx.configs['flat/typescript'],
+  nx.configs['flat/javascript'],
+  nx.configs['flat/angular'],
+  plugins,
+  angularESLint,
+  angularESLintTemplate,
+  declarationsESLint,
+  vitestESLint,
+  storyBookESLint,
+  nxLintBoundaries,
+  {
+    ignores: ['**/dist', 'node_modules/', '**/.nx', '**/.angular']
+  }
+);
